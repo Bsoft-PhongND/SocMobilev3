20f93b128c4137d0f69e96e270eceeb8c9497b5d
nguyendinhphongdx <phongnguyendx@gmail.com>
2022-02-09 17:05:08 +0700

    world map

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,17 @@
-import {Text, View} from 'native-base';
+import {
+  Center,
+  HStack,
+  Image,
+  ScrollView,
+  Skeleton,
+  Text,
+  useMediaQuery,
+  View,
+  VStack,
+} from 'native-base';
 import React from 'react';
 import HeaderMenu from '../../components/headermenu';
+import SkeletonControl from '../../components/skeleton';
 import ViewBackGround from '../../components/viewbackground';
 
 export default function Container() {
@@ -10,8 +21,16 @@ function HomeScreen() {
   return (
     <ViewBackGround>
       <View style={{flex: 1}}>
-        <HeaderMenu title="Home" />
-        <Text style={{color: 'white'}}>Cart Screen</Text>
+        <HeaderMenu title="Dashboard" />
+        <ScrollView style={{paddingHorizontal:10}}>
+          <VStack space={5}>
+            <Image
+              source={require('../../assets/images/world.png')}
+              style={{width: '100%', height: 200}}
+              alt="World"
+            />
+          </VStack>
+        </ScrollView>
       </View>
     </ViewBackGround>
   );
