b82efdd85e40ea1b6f45efe1c66d23caa93d1c3f
nguyendinhphongdx <phongnguyendx@gmail.com>
2022-02-10 17:07:24 +0700

    add carolsel

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,9 +10,13 @@ import {
   VStack,
 } from 'native-base';
 import React from 'react';
+import Carousel from '../../components/carousel';
+import CircularProgressBarComponent from '../../components/circleprogress';
 import HeaderMenu from '../../components/headermenu';
 import SkeletonControl from '../../components/skeleton';
 import ViewBackGround from '../../components/viewbackground';
+import { theme } from '../../theme/theme';
+import wordApp from '../../utils/word';
 
 export default function Container() {
   return <HomeScreen />;
@@ -29,6 +33,21 @@ function HomeScreen() {
               style={{width: '100%', height: 200}}
               alt="World"
             />
+             <Text style={{...theme.fontSize .h3,color: 'white' }}>Top canh bao</Text>
+            <HStack
+              style={{
+                justifyContent: 'space-between',
+                paddingVertical: 0,
+              }}>
+              <CircularProgressBarComponent title={wordApp.hight} value={50} color={theme.colors.hight}/>
+              <CircularProgressBarComponent
+                title={wordApp.medium}
+                value={120}
+                color={theme.colors.medium}
+              />
+              <CircularProgressBarComponent title={wordApp.low} value={60} color={theme.colors.low} />
+            </HStack>
+            <Carousel/>
           </VStack>
         </ScrollView>
       </View>
